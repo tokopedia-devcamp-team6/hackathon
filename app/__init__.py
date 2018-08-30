c3b35292a2036e193232a6191810a8c3b07dfa6c
arizbuno <arizbuno@gmail.com>
2018-08-30 23:48:05 +0700

    stash

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
---
+++ app/addProduct.py
@@ -0,0 +1,6 @@
+from app import app, Resource, db, request
+from app import models as m
+
+class AddProduct(Resource):
+  def post(self):
+    pass
