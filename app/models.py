c64726b2f92e526410c97fe1cd9874988d900023
melvandito <oktavianmelvandito@gmail.com>
2018-08-30 15:16:57 +0700

    add model

--- app/models.py
+++ app/models.py
@@ -19,3 +19,46 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Kategori(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nama = db.Column(db.String(20), index=True, nullable=False)
+    detail = db.Column(db.String(254), nullable=False)
+
+class Produsen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nama_lengkap = db.Column(db.String(80), index=True, nullable=False)
+    nama usaha = db.Column(db.String(80),  nullable=False)
+    email = db.Column(db.String(254), index=True, nullable=False)
+    alamat = db.Column(db.String(200), nullable=False)
+    telepon = db.Column(db.String(14), nullable=False)
+    waktu_mulai = db.Column(db.DateTime, index=True)
+    kategori_id = db.Column(db.Integer, db.ForeignKey('kategori.id'))
+
+class Pembeli:
+    id = db.Column(db.Integer, primary_key=True)
+    nama_lengkap = db.Column(db.String(80), index=True, nullable=False)
+    email = db.Column(db.String(254), index=True, nullable=False)
+    alamat = db.Column(db.String(200), nullable=False)
+    telepon = db.Column(db.String(14), nullable=False)
+
+class Produk:
+    id = db.Column(db.Integer, primary_key=True)
+    nama = db.Column(db.String(80), index=True, nullable=False)
+    stok = db.Column(db.Integer, index=True, nullable=False)
+    harga = db.Column(db.Integer, index=True, nullable=False)
+    detail = db.Column(db.String(254), nullable=False)
+    gambar = db.Column(db.String(254), index=True, nullable=False)
+    cakupan = db.Column(db.String(50), index=True, nullable=False)
+    kategori_id = db.Column(db.Integer, db.ForeignKey('kategori.id'))
+    produsen_id = db.Column(db.Integer, db.ForeignKey('produsen.id'))
+
+class Pesanan:
+    id = db.Column(db.Integer, primary_key=True)
+    jumlah = db.Column(db.Integer, index=True, nullable=False)
+    waktu_butuh = db.Column(db.DateTime, index=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String(30), index=True, nullable=False)
+    produk_id = db.Column(db.Integer, db.ForeignKey('produk.id'))
+    pembeli_id = db.Column(db.Integer, db.ForeignKey('pembeli.id'))
