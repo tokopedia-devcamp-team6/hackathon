6da51053871379427b318ba0463752ddff742cd7
melvandito <oktavianmelvandito@gmail.com>
2018-08-30 23:52:54 +0700

    remove semicolon

--- app/routes.py
+++ app/routes.py
@@ -55,27 +55,27 @@ class Generate(Resource):
 
         # pesanan
         k = Pesanan(id=1, produk_id=3, jumlah=6, waktu_butuh='10/1/2018', status='accepted', pembeli_id=3)
-        db.session.add(k);
+        db.session.add(k)
         k = Pesanan(id=2, produk_id=4, jumlah=7, waktu_butuh='9/13/2018', status='requested', pembeli_id=3)
-        db.session.add(k);
+        db.session.add(k)
         k = Pesanan(id=3, produk_id=1, jumlah=11, waktu_butuh='9/22/2018', status='accepted', pembeli_id=2)
-        db.session.add(k);
+        db.session.add(k)
         k = Pesanan(id=4, produk_id=1, jumlah=9, waktu_butuh='10/16/2018', status='requested', pembeli_id=5)
-        db.session.add(k);
+        db.session.add(k)
         k = Pesanan(id=5, produk_id=2, jumlah=9, waktu_butuh='10/13/2018', status='accepted', pembeli_id=4)
-        db.session.add(k);
+        db.session.add(k)
 
         # produk
         k = Produk(id=1, stok=4, harga=13931, kategori_id=3, produsen_id=4, detail='abon', gambar='/srcabon-ayam-2.jpg', cakupan='semarang')
-        db.session.add(k);
+        db.session.add(k)
         k = Produk(id=2, stok=11, harga=29801, kategori_id=3, produsen_id=5, detail='abon', gambar='/src/abon-ayam.jpg', cakupan='semarang')
-        db.session.add(k);
+        db.session.add(k)
         k = Produk(id=3, stok=14, harga=18005, kategori_id=3, produsen_id=1, detail='abon', gambar='/src/abon-sapi.jpg', cakupan='semarang')
-        db.session.add(k);
+        db.session.add(k)
         k = Produk(id=4, stok=5, harga=32426, kategori_id=1, produsen_id=4, detail='buah', gambar='/src/alpukat.jpeg', cakupan='bandung')
-        db.session.add(k);
+        db.session.add(k)
         k = Produk(id=5, stok=11, harga=14016, kategori_id=1, produsen_id=4, detail='buah', gambar='/src/apel-hijau.jpg', cakupan='jakarta')
-        db.session.add(k);
+        db.session.add(k)
 
         db.session.commit()
         return {'message': 'added'}
